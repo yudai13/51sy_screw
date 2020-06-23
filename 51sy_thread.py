97b995ca787262310acefa2d1f2e42c12baed556
yudai13 <yudai121324@gmail.com>
2020-06-23 19:00:42 +0900

    first commit

---
+++ 51sy_thread.py
@@ -0,0 +1,21 @@
+"""パラメータ設定"""
+
+HEAD = 1  #HEAD1=1、HEAD2=2
+SP = 1  #SP1加工=1、SP2加工=2
+
+THREAD_NAME = 0 #管用平行ねじ=0、管用テーパねじ=1、メートルねじ=2、ユニファイねじ=3、アメリカ管用テーパねじ=4
+THREAD_TYPE = 0 #おねじ=0、めねじ=1
+
+
+
+class ThreadInsert:
+    def __init__(self,s):
+        self.s = s  #刃先までの距離(mm)
+
+class Thread:
+    def __init__(self):
+        self.
+        self.
+        self.
+        self.
+        self.
