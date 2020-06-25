f59abf75ec25426b9be5f57624ff1a5093d8643b
yudai13 <yudai121324@gmail.com>
2020-06-25 19:00:39 +0900

    3

--- 51sy_thread.py
+++ 51sy_thread.py
@@ -5,18 +5,21 @@ SP = 1          #SP1加工=1、SP2加工=2
 POSITION = 20   #ワーク左側端面をz=0.0とした時の右側端面のz位置
 MARGIN = 2      #位置決め余裕の距離
 
-THREAD_NAME = 0       #管用平行ねじ=0、管用テーパねじ=1、メートルねじ=2、ユニファイねじ=3、アメリカ管用テーパねじ=4
-THREAD_TYPE = 0       #おねじ=0、めねじ=1
+THREAD_NAME = 1       #管用平行ねじ=0、管用テーパねじ=1、メートルねじ=2、ユニファイねじ=3、アメリカ管用テーパねじ=4
+THREAD_TYPE = 1       #おねじ=0、めねじ=1
 OUTER_DIAMETER =15    #おねじ外径、もしくはめねじの谷径
 ROOT_DIAMETER = 7.77  #おねじの谷径、もしくはめねじの内径
-LENGTH = 20
-PITCH = 1
+LENGTH = 10
+PITCH = 0.8
 
 S = 1
 
 VELOCITY = 60
 FEED =0.2
-AP = 0.05 #片肉切り込み量
+AP = 0.025 #片肉切り込み量
+
+STANDARD_LENGTH = 4
+EFFECTIVE_THREAD_LENGTH = 2.5
 
 import math
 
@@ -27,25 +30,28 @@ class ThreadInsert:
 
 
 class WorkGeometry:
-    def __init__(self,outer_diameter,root_diameter,length,pitch,tread_name,thread_type):
+    def __init__(self,outer_diameter,root_diameter,length,pitch,tread_name,thread_type,standard_length,effective_thread_length):
         self.outer_diameter = outer_diameter
         self.root_diameter = root_diameter
         self.length = length
         self.pitch = pitch
         self.thread_name = tread_name
         self.thread_type = thread_type
+        self.standard_length = standard_length
+        self.effective_thread_length = effective_thread_length
 
 class CuttingCondition:
     def __init__(self,velocity,feed,ap):
-        self.outer_rpm = 1000*velocity/(math.pi * WorkGeometry(OUTER_DIAMETER,ROOT_DIAMETER,LENGTH,PITCH,THREAD_NAME,THREAD_TYPE).outer_diameter)
-        self.inner_rpm = 1000*velocity/(math.pi * WorkGeometry(OUTER_DIAMETER,ROOT_DIAMETER,LENGTH,PITCH,THREAD_NAME,THREAD_TYPE).root_diameter)
+        self.outer_rpm = 1000*velocity/(math.pi * WorkGeometry(OUTER_DIAMETER,ROOT_DIAMETER,LENGTH,PITCH,THREAD_NAME,THREAD_TYPE,STANDARD_LENGTH,EFFECTIVE_THREAD_LENGTH).outer_diameter)
+        self.inner_rpm = 1000*velocity/(math.pi * WorkGeometry(OUTER_DIAMETER,ROOT_DIAMETER,LENGTH,PITCH,THREAD_NAME,THREAD_TYPE,STANDARD_LENGTH,EFFECTIVE_THREAD_LENGTH).root_diameter)
         self.feed = feed
         self.ap = ap
 
 class ThreadProcess:
     def __init__(self,head,sp,margin,position):
+        self.thread_insert = ThreadInsert(S)
         self.cutting_condition = CuttingCondition(VELOCITY,FEED,AP)
-        self.work_geometry = WorkGeometry(OUTER_DIAMETER,ROOT_DIAMETER,LENGTH,PITCH,THREAD_NAME,THREAD_TYPE)
+        self.work_geometry = WorkGeometry(OUTER_DIAMETER,ROOT_DIAMETER,LENGTH,PITCH,THREAD_NAME,THREAD_TYPE,STANDARD_LENGTH,EFFECTIVE_THREAD_LENGTH)
         self.head = head
         self.sp = sp
         self.margin = margin
@@ -60,35 +66,73 @@ G18\n"
         print(program1)
         
     def positioning(self):
-        program2 = f"Z{float(self.position+self.margin if SP==1 else 0.0-margin)}Y0M{91 if SP==1 else 191}\n\
+        program2 = f"Z{float(self.position+self.margin if SP==1 else 0.0-self.margin)}Y0M{91 if SP==1 else 191}\n\
 X{float(self.work_geometry.outer_diameter+1.0 if self.work_geometry.thread_type==0 else self.work_geometry.root_diameter-1.0)}\n"
         print(program2)
 
     def cutting(self):
         total_path=[]
-        for n in range(0,int((self.work_geometry.outer_diameter-self.work_geometry.root_diameter)//(2*self.cutting_condition.ap))+1):
-            total_path.append(f"X{self.work_geometry.outer_diameter - 2*n*self.cutting_condition.ap}\n")
-        total_path.append(f"X{self.work_geometry.root_diameter}")
+        if self.work_geometry.thread_type==0:
+            for n in range(1,int((self.work_geometry.outer_diameter-self.work_geometry.root_diameter)//(2*self.cutting_condition.ap))+1):
+                total_path.append(f"X{round((self.work_geometry.outer_diameter - 2*n*self.cutting_condition.ap),3)}\n")
+            total_path.append(f"X{round(self.work_geometry.root_diameter,3)}")
+    
+        else:
+            for n in range(1,int((self.work_geometry.outer_diameter-self.work_geometry.root_diameter)//(2*self.cutting_condition.ap))+1):
+                total_path.append(f"X{round((self.work_geometry.root_diameter + 2*n*self.cutting_condition.ap),3)}\n")
+            total_path.append(f"X{round(self.work_geometry.outer_diameter,3)}")
         thread_cycle = "".join(total_path)
 
-
-        
-        program3 = f"G92X{self.work_geometry.outer_diameter if self.work_geometry.thread_type==0 else self.work_geometry.root_diameter}\
-Z{self.position-self.work_geometry.length if self.sp==1 else self.work_geometry.length}F{float(self.work_geometry.pitch)}\n\
+        program3 = f"G92X{float(self.work_geometry.outer_diameter if self.work_geometry.thread_type==0 else self.work_geometry.root_diameter)}\
+Z{float(self.position-self.work_geometry.length-self.thread_insert.s if self.sp==1 else self.work_geometry.length+self.thread_insert.s)}F{float(self.work_geometry.pitch)}\n\
 {thread_cycle}"
         print(program3)
 
 
     def returning(self):
-        program4 = "G0X100.0\n\
-G28U0Y0\n"
+        program4 = f"G0X100.0\n\
+Z{50.0 if self.sp==1 else -50.0}\n\
+G28{'U0Y0' if self.head==1 else 'U0'}\n"
         print(program4)
 
+
+class TaperThreadProcess(ThreadProcess):
+    def cutting(self):
+
+        l = self.work_geometry.standard_length + self.work_geometry.effective_thread_length + self.margin + self.thread_insert.s
+        h = 0.5*l/16
+        a_x = self.work_geometry.outer_diameter
+        a_z = self.position - self.work_geometry.standard_length
+        start_x =self.work_geometry.outer_diameter - (self.position + self.margin)/16
+        start_z = self.position + self.margin
+        total_path=[]
+        if self.work_geometry.thread_type==0:
+            for n in range(1,int((self.work_geometry.outer_diameter-self.work_geometry.root_diameter)//(2*self.cutting_condition.ap))+1):
+                total_path.append(f"X{round((self.work_geometry.outer_diameter - 2*n*self.cutting_condition.ap),3)}\n")
+            total_path.append(f"X{round(self.work_geometry.root_diameter,3)}")
+    
+        else:
+            for n in range(1,int((self.work_geometry.outer_diameter-self.work_geometry.root_diameter)//(2*self.cutting_condition.ap))+1):
+                total_path.append(f"X{round((self.work_geometry.root_diameter + 2*n*self.cutting_condition.ap),3)}\n")
+            total_path.append(f"X{round(self.work_geometry.outer_diameter,3)}")
+        thread_cycle = "".join(total_path)
+
+        program3 = f"G92X{float(self.work_geometry.outer_diameter if self.work_geometry.thread_type==0 else self.work_geometry.root_diameter)}\
+Z{float(self.position-self.work_geometry.length-self.thread_insert.s if self.sp==1 else self.work_geometry.length+self.thread_insert.s)}\
+R{}FFFFF{float(self.work_geometry.pitch)}\n\
+{thread_cycle}"
+        print(program3)
+
+
 def main():
-   thread_process = ThreadProcess(HEAD,SP,MARGIN,POSITION)
-   thread_process.tool_select()
-   thread_process.positioning()
-   thread_process.cutting()
-   thread_process.returning()
+    if THREAD_NAME == 1:
+        thread_process = TaperThreadProcess(HEAD,SP,MARGIN,POSITION)
+    else:
+        thread_process = ThreadProcess(HEAD,SP,MARGIN,POSITION)
+    thread_process.tool_select()
+    thread_process.positioning()
+    thread_process.cutting()
+    thread_process.returning()
+   
 if __name__ == "__main__":
     main()
