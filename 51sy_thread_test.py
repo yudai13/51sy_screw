60bf9736b39243efbd069777170271192162796c
yudai13 <yudai121324@gmail.com>
2020-07-03 18:49:14 +0900

    strategy

--- 51sy_thread_test.py
+++ 51sy_thread_test.py
@@ -1,43 +1,55 @@
-"""パラメータ設定"""
-
-SCREW_NUMB = 1       #管用平行ねじ=0、管用テーパねじ=1、メートルねじ=2、ユニファイねじ=3、アメリカ管用テーパねじ=4
-THREAD_TYPE=1  #おねじ=0、めねじ=1
-DATABASE_NAME = "screw_standard"
-SCREW_NAME = '"R1/8"'
-
 """
-
-HEAD = 1        #HEAD1=1、HEAD2=2
-SP = 1          #SP1加工=1、SP2加工=2
-
+パラメータ
+"""
+THREAD_TYPE = 1
+SCREW_NAME = "R1/8"
+DATABASENAME = "screw_standard"
+HEAD = 1    #HEAD1=1、HEAD2=2
+SP = 1      #SP1加工=1、SP2加工=2
 MARGIN = 2      #位置決め余裕の距離
-
-
-
-OUTER_DIAMETER =15    #おねじ外径、もしくはめねじの谷径
-ROOT_DIAMETER = 7.77  #おねじの谷径、もしくはめねじの内径
 LENGTH = 10
+DIAMETER =10
 PITCH = 0.8
-
 S = 1
-
 VELOCITY = 60
 FEED =0.2
 AP = 0.025 #片肉切り込み量
 
-"""
 
 
 
-
-
-#import math
+import math
 import sqlite3
 import pandas as pd
 
-class ScrewStandard: 
-    def __init__(self,thread_type):
 
+"""設定"""
+
+#抽象
+class Setting:
+    def __init__(self,head,sp,margin):
+        pass
+    
+#具体
+class ThreadSetting(Setting):
+    def __init__(self,head,sp,margin):
+        self.head = head
+        self.sp = sp
+        self.margin = margin
+        
+"""ねじ規格"""
+#抽象
+class Standard:
+    def __init(self):
+        pass
+
+    def search_parameter():
+        pass
+
+#具体
+class GStandard(Standard):
+    def __init__(self,database_name,thread_type):
+        self.database_name = database_name
         self.thread_type = thread_type
         self.name = None
         self.p = None
@@ -48,11 +60,10 @@ class ScrewStandard:
         self.l = None
         self.t = None
 
-    def search_parameter(self,database_name, screw_numb,screw_name):
-        table = ["G","R","M","U","N"]
-        conn = sqlite3.connect(database_name)
+    def search_parameter(self,screw_name):
+        conn = sqlite3.connect(self.database_name)
         c = conn.cursor()
-        df = pd.read_sql(f"select name,p,d_D,d1_D1,a,f,l,t from {table[screw_numb]} where name={screw_name}", conn)
+        df = pd.read_sql(f"select name,p,d_D,d1_D1,a,f,l,t from G where name={screw_name}", conn)
         conn.commit()
         conn.close()
         
@@ -65,13 +76,180 @@ class ScrewStandard:
         self.l = df.at[0,"l"]
         self.t = df.at[0,"t"]
 
+#具体
+class RStandard(Standard):
+    def __init__(self,a):
+        pass
 
-class WorkGeometry:
-    def __init__(self,length):
-        self.screw_standard = ScrewStandard(DBNAME)
-        self.length = length
-        self.thread_name = tread_name
-        self.thread_type = thread_type
+    def search_standard(self):
+        pass
+
+#具体
+class MStandard(Standard):
+    def __init__(self,a):
+        pass
+
+    def search_standard(self):
+        pass
+
+
+#具体
+class UStandard(Standard):
+    def __init__(self,a):
+        pass
+
+
+    def search_standard(self):
+        pass
+
+
+#具体
+class NStandard(Standard):
+    def __init__(self,a):
+        pass
+
+
+    def search_standard(self):
+        pass
+
+        
+        
+        
+        
+        
+
+""" ワーク """
+#抽象
+class Work:
+    def __init__(self,length,diameter):
+        pass
+    
+#具体
+class ThreadWork(Work):
+    def __init__(self,length,diameter,standard):
+       self.length = length
+       self.diameter = diameter
+
+       self.standard = standard
+        
+
+
+"""切削条件"""
+#抽象
+class CuttingCondition:
+    def __init__(self,work,velocity,feed,ap):
+        pass
+
+class ThreadCuttingCondition(CuttingCondition):
+    def __init__(self,work,velocity,feed,ap):
+        self.work = work
+        self.rpm = 1000*velocity/(math.pi * work.diameter)
+        self.ap = ap
+    
+
+"""加工工程"""
+# プログラムの出力を抽象化したクラス(抽象戦略)
+class Process:
+    def __init__(self):
+        pass
+
+    def tool_select(self):
+        pass
+
+    def positioning(self):
+        pass
+
+    def cutting(self):
+        pass
+
+    def returning(self):
+        pass
+    
+    def output_program(self):
+        raise 'Called abstract method !!'
+
+
+
+#具体戦略
+class ThreadProcess(Process):
+    def __init__(self):
+        self.program1 = None
+        self.program2 = None
+        self.program3 = None
+        self.program4 = None
+    
+   
+    def tool_select(self,program):
+        self.program1 = f"N3G10P0Z0\n\
+G0G97G99S{int(program.cutting_condition.rpm)}\
+T303M{93 if program.setting.sp==1 else 193}M{28 if program.setting.head==1 else 128}P{11 if program.setting.sp==1 else 21}\n\
+G18\n"
+        return self.program1
+        
+    def positioning(self,program):
+        program2 = f"Z{float(program.work.length+program.setting.margin if program.setting.sp==1 else -program.setting.margin)}\
+{'Y0.0' if program.setting.head==1 else ''}M{91 if program.setting.sp==1 else 191}\n\
+X{float(program.work.diameter+1.0 if program.work.standard.thread_type==0 else program.work.diameter-1.0)}\n"
+        return self.program2
+
+    def cutting(self,program):
+        total_path=[]
+        if self.work_geometry.thread_type==0:
+            for n in range(1,int((self.work_geometry.outer_diameter-self.work_geometry.root_diameter)//(2*self.cutting_condition.ap))+1):
+                total_path.append(f"X{round((self.work_geometry.outer_diameter - 2*n*self.cutting_condition.ap),3)}\n")
+            total_path.append(f"X{round(self.work_geometry.root_diameter,3)}")
+    
+        else:
+            for n in range(1,int((self.work_geometry.outer_diameter-self.work_geometry.root_diameter)//(2*self.cutting_condition.ap))+1):
+                total_path.append(f"X{round((self.work_geometry.root_diameter + 2*n*self.cutting_condition.ap),3)}\n")
+            total_path.append(f"X{round(self.work_geometry.outer_diameter,3)}")
+        thread_cycle = "".join(total_path)
+
+        self.program3 = f"G92X{float(self.work_geometry.outer_diameter if self.work_geometry.thread_type==0 else self.work_geometry.root_diameter)}\
+Z{float(self.position-self.work_geometry.length-self.thread_insert.s if self.sp==1 else self.work_geometry.length+self.thread_insert.s)}F{float(self.work_geometry.pitch)}\n\
+{thread_cycle}"
+        return self.program3
+
+
+    def returning(self,program):
+        self.program4 = f"G0X100.0\n\
+Z{50.0 if self.sp==1 else -50.0}\n\
+G28{'U0Y0' if self.head==1 else 'U0'}\n"
+        return self.program4
+    
+    def output_program(self,program):
+        print(self.program1)
+        print(self.program2)
+        print(self.program3)
+        print(self.program4)
+
+
+
+
+#NCプログラムを表す(コンテキスト)
+class Program:
+    def __init__(self,setting,work,cutting_condition,process):
+        self.setting = setting
+        self.work = work
+        self.cutting_condition = cutting_condition
+        self.process = process
+
+    def tool_select(self):
+        self.process.tool_select(self)
+
+    def positioning(self):
+        self.process.positioning(self)
+
+    def cutting(self):
+        self.process.cutting(self)
+
+    def returning(self):
+        self.process.returning(self)
+
+    def output_program(self):
+        self.process.output_program(self)
+
+
         
 """
 class CuttingCondition:
@@ -171,11 +349,24 @@ R{-self.h if self.work_geometry.thread_type==0 else self.h}F{float(self.work_geo
 
 """
 def main():
+    setting = ThreadSetting(HEAD,SP,MARGIN)
+    standard = GStandard(DATABASENAME,THREAD_TYPE)
+    work = ThreadWork(LENGTH,DIAMETER,standard)
+    cutting_condition = ThreadCuttingCondition(work,VELOCITY,FEED,AP)
+    
+    thread_program = Program(setting,work,cutting_condition,ThreadProcess())
+    thread_program.tool_select()
+    thread_program.positioning()
+    thread_program.cutting()
+    thread_program.returning()
+    thread_program.output_program()
+
+    """
     screw_standard = ScrewStandard(THREAD_TYPE)
     screw_standard.search_parameter(DATABASE_NAME,SCREW_NUMB,SCREW_NAME)
     print(screw_standard.p)
 
-"""    
+    
     work_geometry = WorkGeometry(LENGTH,THREAD_NAME,THREAD_TYPE)
     p=work_geometry.screw_standard.p
     print(p)
